57d8050b3f953a33077c852da716584d37a85038
Glitch (u46) <none>
2020-04-18 17:10:32 +0000

    🚐⏳ Updated with Glitch

--- breakout.js
+++ breakout.js
@@ -13,6 +13,7 @@ var Breakout = new Phaser.Class({
 
   preload: function() {
     this.load.svg('lander', 'images/combined-lander.svg');
+    this.load.svg('thrust', 'images/thrust.svg');
 
     this.load.atlas(
       "assets",
@@ -23,8 +24,15 @@ var Breakout = new Phaser.Class({
 
   create: function() {
     //  Enable world bounds, but disable the floor
-    this.physics.world.setBoundsCollision(true, true, true, false);
+    this.physics.world.setBoundsCollision(true, true, true, true);
 
+    // debug text
+    this.debugText = this.add.text(0, 0, 'Hello World', { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif' });
+    
+    // thrust image
+    this.thrust1 = this.add.sprite(200, 200, 'thrust');
+    this.thrust1.visible = false;
+    
     //  Create the bricks in a 10x6 grid
     this.bricks = this.physics.add.staticGroup({
       key: "assets",
@@ -43,12 +51,14 @@ var Breakout = new Phaser.Class({
     this.ball = this.physics.add
       .image(400, 500, "lander")
       .setCollideWorldBounds(true)
-      .setBounce(1);
+      .setBounce(0.5);
+    this.ball.setGravityY(200);
+    this.ball.setDrag(10, 10);
     this.ball.setData("onPaddle", true);
 
-    this.paddle = this.physics.add
+    /*this.paddle = this.physics.add
       .image(400, 550, "assets", "paddle1")
-      .setImmovable();
+      .setImmovable();*/
 
     //  Our colliders
     this.physics.add.collider(
@@ -71,11 +81,11 @@ var Breakout = new Phaser.Class({
       "pointermove",
       function(pointer) {
         //  Keep the paddle within the game
-        this.paddle.x = Phaser.Math.Clamp(pointer.x, 52, 748);
+//         this.paddle.x = Phaser.Math.Clamp(pointer.x, 52, 748);
 
-        if (this.ball.getData("onPaddle")) {
-          this.ball.x = this.paddle.x;
-        }
+//         if (this.ball.getData("onPaddle")) {
+//           this.ball.x = this.paddle.x;
+//         }
       },
       this
     );
@@ -84,7 +94,7 @@ var Breakout = new Phaser.Class({
       "pointerup",
       function(pointer) {
         if (this.ball.getData("onPaddle")) {
-          this.ball.setVelocity(-75, -300);
+          // this.ball.setVelocity(-75, -300);
           this.ball.setData("onPaddle", false);
         }
       },
@@ -101,8 +111,8 @@ var Breakout = new Phaser.Class({
   },
 
   resetBall: function() {
-    this.ball.setVelocity(0);
-    this.ball.setPosition(this.paddle.x, 500);
+    // this.ball.setVelocity(0);
+    // this.ball.setPosition(this.paddle.x, 500);
     this.ball.setData("onPaddle", true);
   },
 
@@ -117,22 +127,52 @@ var Breakout = new Phaser.Class({
   hitPaddle: function(ball, paddle) {
     var diff = 0;
 
-    if (ball.x < paddle.x) {
-      //  Ball is on the left-hand side of the paddle
-      diff = paddle.x - ball.x;
-      ball.setVelocityX(-10 * diff);
-    } else if (ball.x > paddle.x) {
-      //  Ball is on the right-hand side of the paddle
-      diff = ball.x - paddle.x;
-      ball.setVelocityX(10 * diff);
-    } else {
-      //  Ball is perfectly in the middle
-      //  Add a little random X to stop it bouncing straight up!
-      ball.setVelocityX(2 + Math.random() * 8);
-    }
+//     if (ball.x < paddle.x) {
+//       //  Ball is on the left-hand side of the paddle
+//       diff = paddle.x - ball.x;
+//       ball.setVelocityX(-10 * diff);
+//     } else if (ball.x > paddle.x) {
+//       //  Ball is on the right-hand side of the paddle
+//       diff = ball.x - paddle.x;
+//       ball.setVelocityX(10 * diff);
+//     } else {
+//       //  Ball is perfectly in the middle
+//       //  Add a little random X to stop it bouncing straight up!
+//       ball.setVelocityX(2 + Math.random() * 8);
+//     }
   },
 
   update: function() {
+    //console.log(this.ball.body.velocity.x);
+    const cursorKeys = this.input.keyboard.createCursorKeys();
+    const thrust = 700;
+    const angleRate = 1.8;
+    
+    this.debugText.setText(`rotation ${this.ball.body.rotation.toFixed(1)} x: ${this.ball.body.x.toFixed(1)} y: ${this.ball.body.y.toFixed(1)}`)
+    if (cursorKeys.up.isDown) {
+      // Thrust!
+      this.thrust1.visible = true;
+      this.thrust1.x = this.ball.body.x;
+      this.thrust1.y = this.ball.body.y;
+      // this.ball.body.applyForce({x: 0, y:100});
+      const rads = this.ball.body.rotation * Math.PI / 180;
+      this.ball.body.acceleration.y = (-thrust) * Math.cos(rads);
+      this.ball.body.acceleration.x = (thrust) * Math.sin(rads);
+    } else {
+      this.thrust1.visible = false;
+      this.ball.body.acceleration.y = 0;
+      this.ball.body.acceleration.x = 0;
+    }
+    
+    if (cursorKeys.left.isDown) {
+      this.ball.body.rotation -= angleRate;
+    }
+
+    if (cursorKeys.right.isDown) {
+      this.ball.body.rotation += angleRate;
+    }
+
+
     if (this.ball.y > 600) {
       this.resetBall();
     }
